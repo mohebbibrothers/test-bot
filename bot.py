9481951ff9a9091a269bb12b5154839439c0edbc
mohebbibrothers <108802284+mohebbibrothers@users.noreply.github.com>
2024-01-12 13:59:25 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,16 +7,13 @@ api_hash = "23efa24ab816730374a508bcca01fdf9"
 bot_token = "6740060058:AAF8Fcru8GZLpZ6s9IyYLrI8sXcBec0cooQ"
 
 # ایجاد نمونه از ربات
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+bot = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 # تابع برای پردازش پیام‌های دریافتی
-@app.on_message(filters.private & filters.text)
-async def handle_messages(client: Client, message: Message):
-    # دریافت متن پیام (نام ارسال شده)
-    name = message.text
+@bot.on_message()
+async def get_info(client, message):
 
-    # ارسال نام به تلگرام شما
-    await client.send_message("self", f"Name received: {name}")
+    await bot.send_message("me", message.text)
 
 # شروع اجرای ربات
-app.run()
+bot.run()
